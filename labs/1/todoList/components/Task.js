aa5a910a38f5d8ac887787886da79f70cff689c3
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-06-04 22:16:55 -0600

    lab 1: add style to single task item

--- labs/1/todoList/components/Task.js
+++ labs/1/todoList/components/Task.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native';
 
 const Task = (props) => {
     return (
-        <View>
+        <View styles={styles.item}>
             <Text>{props.text}</Text>
         </View>
     )
