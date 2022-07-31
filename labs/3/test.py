040e14960170804fe75fbf6b844e2365d64f89e7
Eduardo Quiros <eduardo@eduardoquiros.com>
2022-07-31 00:06:46 -0600

    agregar select inicial

--- labs/3/test.py
+++ labs/3/test.py
@@ -1,8 +1,12 @@
 from selenium import webdriver
 import time
 
+from selenium.webdriver.common.by import By
+
 driver = webdriver.Firefox()
 driver.get("https://www.bnventadebienes.com/Home/HomeFilter")
+provincia_select = driver.find_element(by=By.ID, value="ProvinceId")
+provincia_select.click()
 time.sleep(10)
 driver.quit()
 print("Test completed")
